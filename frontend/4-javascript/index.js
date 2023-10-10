599f7ce6ad1745ba50eb200d654f3df9b246a2f9
aljeri <n.aljerri@gmail.com>
2023-10-10 00:22:40 -0400

    new file:   Notes/5-Javascript.pdf 	new file:   frontend/4-javascript/functions.html 	new file:   frontend/4-javascript/index.html 	new file:   frontend/4-javascript/index.js 	new file:   frontend/4-javascript/loops.html 	new file:   frontend/4-javascript/starter.html

---
+++ frontend/4-javascript/index.js
@@ -0,0 +1,44 @@
+document.write("<h1>Welcome, ",  visitor,  "</h1>");
+document.write("<h1 style='text-align: center;'>Hello World!!</h1>");
+
+function init(){
+    console.log('Page Loaded');
+}
+let nameN='noura';
+function test() {
+    //reset the whole body to write! not practical
+   // document.write('<h1>'+ name +'</h1>');
+    //better way - update specific id
+    document.getElementById('header').innerText=nameN;
+}
+function submited(){ //without form tag 
+    let email= document.getElementById('email');
+    let name= document.getElementById('name');
+    console.log(name, email);
+    console.log(name.value, email.value);
+
+}
+//with form tag -
+let forma= document.getElementById('myForm');
+forma.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let email= document.getElementById('email');
+    let name= document.getElementById('name');
+    console.log(email.value, name.value);
+    //confirm('thank you')
+    //forma.reset();
+
+});
+
+//quick counting app
+let counterNum=0;
+countID= document.getElementById('count');
+
+function counter(){
+counterNum += 1;
+countID.innerText=counterNum;
+}
+function reset(){
+countID.innerText=0;
+counterNum=0;
+}
